3f684c9173d483dccc3c2abb15bec7f1186e31bb
Pablo <p.c.ramirez5150@gmail.com>
2021-07-13 21:12:11 +0200

    Creates AccountManagement WebApi and returns endpoint informations with Hypermedia Controls

---
+++ AccountManagement.WebApi/Controllers/EntryPointController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AccountManagement.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EntryPointController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public EntryPointController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        public ActionResult<EndpointInfo> Get()
+        {
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}/";
+            return Ok(new EndpointInfo
+            {
+                Href = baseUrl + "accountManagement", 
+                Rel = "self",
+                Links = new List<Link>
+                {
+                    new() {Href = baseUrl + "accountManagement", Rel = "accounts"}
+                }
+            });
+        }
+
+        public class Link
+        {
+            public string Href { get; init; }
+            public string Rel { get; init; }
+        }
+        
+        public class EndpointInfo
+        {
+            public string Href { get; init; }
+            public string Rel { get; init; }
+            public IEnumerable<Link> Links { get; init; }
+        };
+    }
+}
