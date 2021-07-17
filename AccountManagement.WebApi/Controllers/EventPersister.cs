42e6cfc3f55344dc014e5ff5bdfb761529334356
Pablo Company Ramírez <p.c.ramirez5150@gmail.com>
2021-07-18 00:22:11 +0200

    Fixes connection to ESDB

--- AccountManagement.WebApi/Controllers/EventPersister.cs
+++ AccountManagement.WebApi/Controllers/EventPersister.cs
@@ -9,34 +9,27 @@ namespace AccountManagement.WebApi.Controllers
 {
     public interface IEventPersister
     {
-        Task PersistEventAsync(object domainEvent);
+        Task PersistEventAsync<TDomainEvent>(TDomainEvent domainEvent) where TDomainEvent : IEvent;
     }
 
     public class EventPersister : IEventPersister
     {
         private readonly IEventStoreConnection _connection;
-        private readonly ITimeService timeService;
 
-        public EventPersister(ITimeService timeService)
+        public EventPersister()
         {
-            this.timeService = timeService;
-            _connection = EventStoreConnection.Create(new Uri("tcp://admin:changeit@localhost:1113"));
+            var connectionSettings = ConnectionSettings.Create()
+                .DisableTls().DisableServerCertificateValidation().EnableVerboseLogging();
+            _connection = EventStoreConnection.Create(connectionSettings, new Uri("tcp://admin:changeit@localhost:1113"));
             _connection.ConnectAsync().GetAwaiter().GetResult();
         }
 
-
-        public async Task PersistEventAsync(object domainEvent)
+        public async Task PersistEventAsync<TDomainEvent>(TDomainEvent domainEvent) where TDomainEvent : IEvent
         {
-            var eventMetadata = new EventMetadata(timeService.UtcNow, Guid.NewGuid().ToString());
+            var serializedEvent = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(domainEvent));
+            var eventData = new EventData(Guid.NewGuid(), domainEvent.GetType().Name, true, serializedEvent, new byte[0]);
 
-            var eventData = new EventData(
-                Guid.NewGuid(),
-                "DomainEvent",
-                true,
-                Encoding.UTF8.GetBytes(JsonSerializer.Serialize(domainEvent)),
-                Encoding.UTF8.GetBytes(JsonSerializer.Serialize(eventMetadata))
-            );
-            await _connection.AppendToStreamAsync(nameof(BeganFollowing), ExpectedVersion.Any, eventData);
+            await _connection.AppendToStreamAsync("DomainEvent", ExpectedVersion.Any, eventData).ConfigureAwait(false);
         }
     }
 }
